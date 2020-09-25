55955898a7200f83f1a85dfdf31af141af3119ea
Martin Johansen <martinjohansen@dhcp-10-24-137-173.wlan.ntnu.no>
2020-09-25 17:32:47 +0200

    Added trello connector script

---
+++ src/js/connector.js
@@ -0,0 +1,20 @@
+
+console.log('Hello world')
+const GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
+
+const onBtnClick = function (t, opts) {
+  console.log('my button was clicked');
+};
+
+window.TrelloPowerUp.initialize({
+  'card-buttons': async (t, opts) => {
+    return [{
+      // usually you will provide a callback function to be run on button click
+      // we recommend that you use a popup on click generally
+      icon: GRAY_ICON, // don't use a colored icon here
+      text: 'Relative Due Dates',
+      callback: onBtnClick,
+      condition: 'edit'
+    }];
+  }
+});
