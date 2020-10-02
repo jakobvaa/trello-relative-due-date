2fcef9f6810ed3aa35e429d19c38cdf0aaff0be2
Martin Johansen <martinjohansen@Martins-MacBook-Pro.local>
2020-10-02 19:58:09 +0200

    checking api

--- src/js/Popup.js
+++ src/js/Popup.js
@@ -1,6 +1,8 @@
+import Axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
-
+import axios from 'axios'
+const appKey = 'f37ab50db205f3dc8f32dc97971117f4'
 const t = TrelloPowerUp.iframe({
 	appKey: 'f37ab50db205f3dc8f32dc97971117f4',
 	appName: 'relative-due-date'
@@ -9,11 +11,15 @@ const t = TrelloPowerUp.iframe({
 const Popup = (props) => {
 
 	const [cards, setCards] = useState([])
-
+	const [loading, setLoading] = useState(false)
 	useEffect(() => {
 		t.getRestApi().getToken()
 		.then(token => {
-			console.log(token)
+			axios({
+				url: `https://api.trello.com/1/members/me/boards?fields=name,url&key=${appKey}&token=${token}`
+			}).then(boards => {
+				console.log(boards)
+			}) 
 		})
 	})
 	return (
