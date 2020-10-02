f5b97e19d5ebf16be15e995e4446dd979a561f59
Martin Johansen <martinjohansen@Martins-MacBook-Pro.local>
2020-10-02 18:55:07 +0200

    fdf

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,28 +1,43 @@
 console.log('Hello world')
-const GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
+const GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg'
 
 const onBtnClick = (t, opts) => {
-  t.getRestApi().isAuthorized()
-  .then((auth) => {
-    console.log(auth)
-    return t.popup({
-      title: 'Possible cards',
-      items: [{
-        text: 'Test 1'
-      }, {
-        text: 'Test 2'
-      }]
-    })  
+  return t.popup({
+    title: 'Possible cards',
+    items: [{
+      text: 'Test 1'
+    }, {
+      text: 'Test 2'
+    }]
   })
-};
+}
 
-
+const showIrame = (t) => {
+  return t.popup({
+    title: 'Authorize to continue',
+    url: '../html/authorize.html'
+  })
+}
 
 window.TrelloPowerUp.initialize({
-  'card-buttons': (t, opts) => {
-    return [{
-      text: 'Relative due date',
-      callback: onBtnClick
-    }]
+  'card-buttons': function(t) {
+    return t.getRestApi()
+      .isAuthorized()
+      .then(function(isAuthorized) {
+        if (isAuthorized) {
+          return [{
+            text: 'Relative due date',
+            callback: showMenu
+          }];
+        } else {
+          return [{
+            text: 'David\'s Power-Up',
+            callback: showIframe
+          }];
+        }
+      });
   }
-});
+}, {
+  appKey: 'your-app-key',
+  appName: 'My Great Power-Up'
+})
