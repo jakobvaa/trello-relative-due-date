5aed2f182b9e000f333e45ffe8cc59fde3ef2b30
Martin Johansen <martinjohansen@Martins-MacBook-Pro.local>
2020-10-03 01:14:48 +0200

    fdfd

--- src/js/Popup.js
+++ src/js/Popup.js
@@ -1,8 +1,21 @@
 import Axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
+import styled from styled-components
 import axios from 'axios'
 const appKey = 'f37ab50db205f3dc8f32dc97971117f4'
+
+const CardContainer = styled.div`
+	overflow-x: hidden;
+	overflow-y: auto;
+	padding: 0 12px 12px;
+`
+
+const List = styled.ul`
+	list-style: none;
+	margin: 0;
+	padding: 0;
+`
 
 const t = TrelloPowerUp.iframe({
 	appKey: 'f37ab50db205f3dc8f32dc97971117f4',
@@ -39,7 +52,7 @@ const Popup = (props) => {
 
 
 	const renderCards = () => (
-		<div>
+		<div className='js-results'>
 			<ul className='pop-over-list js-list navigable'>
 				{cards.map(card => (
 					<li key={card.id}>
