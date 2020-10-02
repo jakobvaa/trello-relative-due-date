b743fe1d6b492c0031501bad769afd55ea3336ee
Martin Johansen <martinjohansen@Martins-MacBook-Pro.local>
2020-10-03 01:40:08 +0200

    fff

--- src/js/Popup.js
+++ src/js/Popup.js
@@ -68,7 +68,7 @@ const Popup = (props) => {
 			</ul>
 			<span>
 				<button onClick={() => decrement()}>-</button>
-				<input type='number' disabled></input>
+				<input type='number' disabled>{difference}</input>
 				<button onClick={() => increment()}>+</button>
 			</span>
 			<button ref={ref}>Set Relative Due Date</button>
