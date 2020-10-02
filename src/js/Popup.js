2b4674d4188963873f6fa628f3c70c7d6084802b
Martin Johansen <martinjohansen@Martins-MacBook-Pro.local>
2020-10-03 01:22:49 +0200

    fjfjf

--- src/js/Popup.js
+++ src/js/Popup.js
@@ -1,21 +1,9 @@
 import Axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
-import styled from styled-components
+
 import axios from 'axios'
 const appKey = 'f37ab50db205f3dc8f32dc97971117f4'
-
-const CardContainer = styled.div`
-	overflow-x: hidden;
-	overflow-y: auto;
-	padding: 0 12px 12px;
-`
-
-const List = styled.ul`
-	list-style: none;
-	margin: 0;
-	padding: 0;
-`
 
 const t = TrelloPowerUp.iframe({
 	appKey: 'f37ab50db205f3dc8f32dc97971117f4',
@@ -50,7 +38,7 @@ const Popup = (props) => {
 		}
 	}, [cards])
 
-
+	console.log(selected)
 
 	const renderCards = () => (
 		<div className='js-results'>
