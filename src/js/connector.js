cf483d9ab669ef3ffbe7f6009b94835ce77d5c99
Martin Johansen <martinjohansen@dhcp-10-24-138-46.wlan.ntnu.no>
2020-10-09 11:16:40 +0200

    fdfd

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,7 +3,7 @@ const GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-
 const BASE_URL = 'https://api.trello.com/1/'
 const appKey = 'f37ab50db205f3dc8f32dc97971117f4'
 const appName = 'relative-due-date'
-const checkBoard = require('./boardFunctions')
+const { checkBoard } = require('./boardFunctions')
 
 const onBtnClick = (t, opts) => {
   t.getRestApi().getToken()
