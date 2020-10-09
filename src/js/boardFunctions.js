c330e05e5249a7da140156e717d6a0c6e1919e68
Martin Johansen <martinjohansen@dhcp-10-24-138-46.wlan.ntnu.no>
2020-10-09 11:47:58 +0200

    fdfd

--- src/js/boardFunctions.js
+++ src/js/boardFunctions.js
@@ -7,9 +7,11 @@ export const checkBoard = async (t, opts) => {
 		url: `/getboard?boardid=${boardId.id}`
 	}) 
 	const trelloIds = trelloCards.map(card => card.id)
+	trelloCards.sort((a,b) => a.id - b.id)
 	const relativeCards = relativeBoard.data.board.filter(card => trelloIds.includes(card.cardId))
-	console.log(relativeCards)
+	relativeCards.sort((a,b ) => a.cardId - b.cardId)
 	console.log(trelloCards)
+	console.log(relativeCards)
 	return [{
 		text: 'Sync Relative Dates'
 	}]
