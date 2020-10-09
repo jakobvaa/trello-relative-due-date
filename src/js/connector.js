ac7b6e865dbc4754d3a17c57d027815741c38c6b
Martin Johansen <martinjohansen@dhcp-10-24-138-46.wlan.ntnu.no>
2020-10-09 18:20:20 +0200

    fixed invalid date change

--- src/js/connector.js
+++ src/js/connector.js
@@ -47,6 +47,14 @@ const verifyCard = async (t) => {
   }
   if(trelloCard.due !== relativeCard.due_date) {
     relativeCard.due_date = trelloCard.due
+    if(isNaN(trelloCard.due) && relativeCard.parent){
+      const {cardId, due_date} = relativeCard
+      await axios({
+        method: 'PUT',
+        url: `${BASE_URL}cards/${cardId}?key=${appKey}&token=${token}&due=${due_date}`
+      })
+      return
+    }
     await axios({
       method: 'POST',
       url: '/updatedate',
