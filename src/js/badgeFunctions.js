25a84ec6a75981496f1373f458c20c5b6fa476f3
Martin Johansen <martinjohansen@Martins-MacBook-Pro.local>
2020-10-25 17:27:28 +0100

    badge function cleanup

--- src/js/badgeFunctions.js
+++ src/js/badgeFunctions.js
@@ -26,10 +26,6 @@ export const verifyCard = async (t) => {
   if(trelloCard.due !== relativeCard.due_date) {
     if(relativeCard.parent){
       const {cardId, due_date, parent, boardId} = relativeCard
-      const parentCard = await axios({
-        url: `/getcard?cardname=${parent}&boardid=${boardId}`
-      })
-
       const token = await t.getRestApi().getToken()
       await axios({
         method: 'PUT',
