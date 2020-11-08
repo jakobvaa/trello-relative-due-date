82bd516fc52059b2e1b63d0a02ff8e5b854ee648
Martin Johansen <martinjohansen1705@gmail.com>
2020-11-08 19:37:56 +0100

    changed calendar library

--- src/js/dateFunctions.js
+++ src/js/dateFunctions.js
@@ -1,32 +1,62 @@
 const ics = require('ics')
+const {Component, Property} = require('immutable-ics')
 const {saveAs} = require('file-saver')
 const generateCalendar = async (t, label) => {
 	const cards = await  t.cards('all')
-	const calendarCards = []
+	const versionProperty = new Property({name: 'VERSION', value: 2})
+	const calendar = new Component({name: 'VCALENDAR'})
+	calendar.pushProperty(versionProperty)
 	cards.forEach(card => {
-		const calendarCard = card.labels.find(lab => lab.name === label)
-		if(calendarCard) {
-			calendarCards.push(calendarCard)
+		const hasLabel = card.labels.find(lab => lab.name === label)
+		if(hasLabel && card.due) {
+			calendarCards.push(card)
 		}
 	})
-	const calendarEvents = calendarCards.map(card => {
-		const dueDate = new Date(card.due)
-		return {
-			start: [
-				dueDate.getFullYear(),
-				dueDate.getMonth(),
-				dueDate.getDate(),
-				10,
-				0
-			],
-			duration: {hours: 1},
-			description: card.desc,
-			url: card.url,
-		}
+	calendarCards.forEach(card => {
+		const event = new Component({name: 'VEVENT'})
+		const start = new Property({
+			name: 'DTSTART',
+			parameters: {VALUE: 'DATE'},
+			value: new Date(card.due)
+		})
+		event.pushProperty(start)
+		const duration = new Property({
+			name: 'DURATION',
+			value: 'PT1H'
+		})
+		event.pushProperty(duration)
+		const description = new Property({
+			name: 'DESCRIPTION',
+			value: card.desc
+		})
+		event.pushProperty(description)
+		const url = new Property({
+			name: 'URL',
+			value: card.url
+		})
+		event.pushProperty(url)
+		calendar.pushComponent(event)
 	})
-	console.log(calendarEvents)
-	const {err, value} = ics.createEvent(calendarEvents)
-	console.log(value)
+	// const calendarEvents = calendarCards.map(card => {
+	// 	const dueDate = new Date(card.due)
+	// 	return {
+	// 		start: [
+	// 			dueDate.getFullYear(),
+	// 			dueDate.getMonth(),
+	// 			dueDate.getDate(),
+	// 			10,
+	// 			0
+	// 		],
+	// 		duration: {hours: 1},
+	// 		description: card.desc,
+	// 		url: card.url,
+	// 	}
+	// })
+	// console.log(calendarEvents)
+	// const {err, value} = ics.createEvent(calendarEvents)
+	// console.log(value)
+	const blob = new Blob([calendar.toString()], {type: 'text/plain;charset=utf-8'})
+	saveAs(blob, `${label}-calendar.ics`)
 }
 
 export const calendarPopup = (t, opts) => {
