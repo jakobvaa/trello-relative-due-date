eb182d314584371f1b5121123fdd9cecd388812c
Martin Johansen <martinjohansen1705@gmail.com>
2020-11-08 19:46:04 +0100

    event and calendar changes

--- src/js/dateFunctions.js
+++ src/js/dateFunctions.js
@@ -4,8 +4,9 @@ const {saveAs} = require('file-saver')
 const generateCalendar = async (t, label) => {
 	const cards = await  t.cards('all')
 	const versionProperty = new Property({name: 'VERSION', value: 2})
-	const calendar = new Component({name: 'VCALENDAR'})
-	calendar.pushProperty(versionProperty)
+	let calendar
+	calendar = new Component({name: 'VCALENDAR'})
+	calendar = calendar.pushProperty(versionProperty)
 	const calendarCards = []
 	cards.forEach(card => {
 		const hasLabel = card.labels.find(lab => lab.name === label)
@@ -14,29 +15,30 @@ const generateCalendar = async (t, label) => {
 		}
 	})
 	calendarCards.forEach(card => {
-		const event = new Component({name: 'VEVENT'})
+		let event
+		event = new Component({name: 'VEVENT'})
 		const start = new Property({
 			name: 'DTSTART',
 			parameters: {VALUE: 'DATE'},
 			value: new Date(card.due)
 		})
-		event.pushProperty(start)
+		event = event.pushProperty(start)
 		const duration = new Property({
 			name: 'DURATION',
 			value: 'PT1H'
 		})
-		event.pushProperty(duration)
+		event = event.pushProperty(duration)
 		const description = new Property({
 			name: 'DESCRIPTION',
 			value: card.desc
 		})
-		event.pushProperty(description)
+		event = event.pushProperty(description)
 		const url = new Property({
 			name: 'URL',
 			value: card.url
 		})
-		event.pushProperty(url)
-		calendar.pushComponent(event)
+		event = event.pushProperty(url)
+		calendar = calendar.pushComponent(event)
 	})
 	// const calendarEvents = calendarCards.map(card => {
 	// 	const dueDate = new Date(card.due)
