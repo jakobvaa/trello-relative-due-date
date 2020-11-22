f0db279c0e43d983ccf41505fc7a84ad75403a1b
Martin Johansen <martinjohansen1705@gmail.com>
2020-11-22 16:31:11 +0100

    added sidebar

--- src/js/Timeline.js
+++ src/js/Timeline.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
 import styled from 'styled-components'
+import TimelineSidebar from './TimelineSidebar'
 
 const Container = styled.div`
 	display: flex;
@@ -15,6 +16,9 @@ const Sidebar = styled.div`
 	width: 10%;
 `
 
+const ignoreList = ['Application, AP-Tech-spons', 'Tech-spons']
+
+
 
 const t = TrelloPowerUp.iframe({
 	appKey: 'f37ab50db205f3dc8f32dc97971117f4',
@@ -27,16 +31,16 @@ const BASE_URL = 'https://api.trello.com/1/'
 const Timeline = (props) => {
 	useEffect(async () => {
 		const board = await t.board('all')
-		const lists = await t.lists('all')
+		const lists = await t.list('all')
 		console.log('board', board)
 		console.log('lists', listst)
 		
 	})
 
 	return (
-		<div>
-			dette er timeline
-		</div>
+		<Container>
+			
+		</Container>
 	)
 }
 
---
+++ src/js/TimelineSidebar.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const SidebarContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 10%;
+	background-color: lightgrey;
+`
+
+const CheckList = styled.div`
+	width: calc(100% - 10px);
+	padding: 5px;
+	margin: 10px 0;
+`
+
+
+
+co
+
+
+const TimelineSidebar = ({labels, checkedLabels, setLabel, unsetLabel}) => {
+
+	const renderLines = () => (
+		labels.map(label => {
+			isChecked = checkedLabels.includes(label.id)
+			return (
+				<div 
+					key={label.id}
+					onClick={isChecked ? unsetLabel(label.id) : setLabel(label.id)}>
+					<input 
+						type='ceckbox' 
+						checked={checkedLabels.includes(label.name)}
+						onChange={() => {return}}/>
+					<label>{label.name}</label>
+				</div>
+			)
+		}
+		)
+	)
+
+	return(
+		<SidebarContainer>
+			<CheckList>
+				{renderLines()}
+			</CheckList>
+		</SidebarContainer>
+	)
+}
+
+export default TimelineSidebar
