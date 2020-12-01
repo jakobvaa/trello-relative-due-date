5d0a8779463dead2f7018d0dd439ad86e5fb44bf
Martin Johansen <martinjohansen1705@gmail.com>
2020-12-01 17:35:38 +0100

    ffd

--- src/js/Timeline.js
+++ src/js/Timeline.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import ReactDOM from 'react-dom'
-
+import moment from 'moment'
 import styled from 'styled-components'
 import TimelineSidebar from './TimelineSidebar'
 import CardTimeline from './CardTimeline'
