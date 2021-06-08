7ff8eeb97f8841f8177994eaf5df8b5db0fc2257
Martin Johansen <martinjohansen1705@gmail.com>
2021-06-08 19:10:08 +0200

    fd

--- src/js/utils.js
+++ src/js/utils.js
@@ -79,7 +79,7 @@ export const verifyRules = async (t, card, list) => {
 						if (item.state !== correspondingRequirement.state) {
 							promises.push(axios({
 								method: 'DELETE',
-								url: `${BASE_URL}checklists/${faultyChecklist.id}/checkItems/${checkItem.id}?key=${appKey}&token=${token}`
+								url: `${BASE_URL}checklists/${faultyChecklist.id}/checkItems/${item.id}?key=${appKey}&token=${token}`
 							}))
 							promises.push({
 								method: 'POST',
