9fc134bd458e391529cbcb728a41515ae31982bf
Martin Johansen <martinjohansen1705@gmail.com>
2021-06-08 14:37:38 +0200

    fdf

--- src/js/utils.js
+++ src/js/utils.js
@@ -13,7 +13,7 @@ export const verifyRules = async (t, card, list) => {
 	console.log(currentChecklists)
 	const l = await t.lists('all')
 	console.log(l)
-	const rulesList = l.find(newList => newList.name.includes(list.name))
+	const rulesList = l.find(newList => newList.name.includes(list.name) && !newList.id === list.id)
 	console.log(rulesList)
 	const { cards } = rulesList
 	cards.forEach(async newCard => {
